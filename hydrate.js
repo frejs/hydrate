65513e42ca83aa3263bc3ee63a8fff853500e644
伊撒尔 <1533540012@qq.com>
2021-09-14 16:49:00 +0800

    Create hydrate.js

---
+++ hydrate.js
@@ -0,0 +1,36 @@
+import { render } from './reconcile'
+
+function createWalker(node) {
+  return document.createTreeWalker(node, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: () => NodeFilter.FILTER_ACCEPT,
+  })
+}
+
+function morph(sourceElement, targetElement) {
+  const sourceWalker = createWalker(sourceElement)
+  const targetWalker = createWalker(targetElement)
+  const walk = fn =>
+    sourceWalker.nextNode() && targetWalker.nextNode() && fn() && walk(fn)
+
+  walk(() => {
+    const { currentNode } = sourceWalker
+    const targetNode = targetWalker.currentNode
+    // TODO more things
+    if (currentNode.tagName === targetNode.tagName) {
+      if (currentNode === document.activeElement) {
+        window.requestAnimationFrame(() => {
+          targetNode.focus()
+        })
+      }
+    }
+    return true
+  })
+}
+
+export default function hydrate(vnode, node, config) {
+  config.hydrate = true
+  const clone = node.cloneNode(false)
+  render(vnode, clone, config)
+  morph(clone, node)
+  node.parentNode.replaceChild(clone, node)
+}
